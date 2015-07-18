651eff103b82d724c688cc86d3f75edced3a868e
louisyoung <ly.louisyoung@gmail.com>
2015-07-18 13:46:15 +0800

    Update opus_demo.c

--- opuslib/src/main/jni/src/opus_demo.c
+++ opuslib/src/main/jni/src/opus_demo.c
@@ -1,6 +1,5 @@
 #include "config.h"
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -9,13 +8,39 @@
 
 void print_use_demo(void)
 {
-	printf("opus_demo for encode and decoe!\n");
+	printf("opus_demo for encode and decode!\n");
 	printf("Usage:\n");
 	printf("opus_demo d|e input output option\n");
 	printf("d\tdecoding\n");
 	printf("e\tencoding\n\n");
+	printf("r\tsimulate audio record\n");
 
 }
+
+void simu_record(char * input, char * output, char * opution)
+{
+	startRecording(output);
+
+	FILE *fp;
+	if((fp = fopen(input, "r")) == NULL )
+	{
+		printf("Open File error!");
+		return;
+	}
+	const unsigned int len = 1920;
+	unsigned int curLen = 0;
+	uint8_t *buffer = (uint8_t *)malloc(sizeof(char) * len);
+	while(!feof(fp))
+	{
+		curLen = fread(buffer, 1, len,fp);
+		writeFrame(buffer, curLen);
+	}
+
+	free(buffer);
+	fclose(fp);
+	stopRecording();
+}
+
 int main(int argc, char *argv[])
 {
 	char *input;
@@ -53,6 +78,9 @@ int main(int argc, char *argv[])
     case 'd':
     	decode(input, output, option);
     	break;
+    case 'r':
+        simu_record(input, output, option);
+        	break;
     default:
     	print_use_demo();
     	break;
