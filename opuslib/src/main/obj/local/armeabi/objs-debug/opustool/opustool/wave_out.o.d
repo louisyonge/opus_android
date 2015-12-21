a80fb2c6fb3f3093d1838eac68d5f88817f8b039
louisyoung <ly.louisyoung@gmail.com>
2015-12-22 00:25:57 +0800

    fixed untracked files

--- opuslib/src/main/obj/local/armeabi/objs-debug/opustool/opustool/wave_out.o.d
+++
@@ -1,5 +0,0 @@
-C:/opus/OpusPlayer/opuslib/src/main//obj/local/armeabi/objs-debug/opustool/opustool/wave_out.o: \
- C:/opus/OpusPlayer/opuslib/src/main//jni/opustool/wave_out.c \
- C:/opus/OpusPlayer/opuslib/src/main//jni/include/config.h
-
-C:/opus/OpusPlayer/opuslib/src/main//jni/include/config.h:
